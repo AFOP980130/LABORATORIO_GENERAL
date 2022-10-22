13f0ef8a9ae9464570ec0babaa7bf9fad1558f52
AFOP980130 <felipeorozcopinilla@gmail.com>
2022-10-21 22:28:22 -0500

    validación de entrada si no es número con try except  y continue

--- Conversor_pesos_dolares.py
+++ Conversor_pesos_dolares.py
@@ -1,25 +1,23 @@
-##Copiando conversor de monedas Platzi
-solonumeros = "Debes ingresar un valor númerico"
-print(" Hola Bienvenido a mi conversor de dolares a pesos ")
+##Andres Orozco primerps pasos
+print(" 🤑🤑🤑 Hola Bienvenido a mi conversor de dolares a pesos 🤑🤑🤑 ")
+solonumeros = ("Debes ingresar un valor númerico 🤞")
 while True:
     try:
-        valor_dolar = float(input("¿Que precio tiene el USD hoy en COP? "))
+        valor_dolar = float(input("¿Que precio tiene el USD hoy en COP? 🤔 "))
     except ValueError:
         print(solonumeros)
         continue
     try:
-        pesos = float(input("¿Cuantos COP quieres convertir? "))
+        pesos = float(input("¿Cuantos COP quieres convertir? 🤔 "))
     except ValueError:
         print(solonumeros)
         continue
     conversion = float(pesos / valor_dolar)
     conversion = round(conversion, 2)
-    print("El resultado es $" + str(conversion) + " dolares Americanos")
-    otra_conversion = str(input("¿Quieres hacer otro calculo Si o No? "))
+    print("El resultado es $" + str(conversion) + " dolares Americanos 🤑👌")
+    otra_conversion = str(input("¿Quieres hacer otro calculo Si o No? 🤔"))
     if otra_conversion != "No" and otra_conversion != "no":
-        print("Nuevo calculo")
+        print("Vamos con un nuevo calculo 😊")
     elif otra_conversion == "No" or otra_conversion == "no":
-        print("Gracias por usar mi primer programa")
+        print("😍😍😍 Gracias por usar mi primer programa 😍😍😍")
         break
-
-
--- conversor_dolares_pesos.py
+++ conversor_dolares_pesos.py
@@ -1,17 +1,27 @@
 ## Conversor de dolares a pesos Colombainos
-print("Bienvenido a mi segundo programa conversor de dolares a pesos")
+print("Bienvenido a mi segundo programa conversor de dolares a pesos 🤑🤑🤑")
 
 ##valor_pesos = int(input("¿Que precio tiene el peso hoy?: "))
-valor_dolar= 4735
+Valordolar = 4735
 
 while True:
-    mis_dolares = float(input("¿Cantidad de dolar a convertir?: "))
-    conversion = float(mis_dolares*valor_dolar)
+    try:
+        mis_dolares = float(input("¿Cantidad de USD a convertir a COP?: 🤑 "))
+    except ValueError:
+        print("""Debes ingresar un valor númerico 🤔""")
+        continue
+    conversion = float(mis_dolares * Valordolar)
     conversion = round(conversion, 2)
-    print("El resultado es  $" + str(conversion) + " pesos Colombianos")
-    otro_calculo = str(input("¿Quieres hacer otro calculo Si o No?: "))
-    if otro_calculo != "No" and otro_calculo != "no":
-        print("Nueva conversión")
+    print("El resultado es  $" + str(conversion) + " pesos Colombianos 😊")
+    try:
+        otro_calculo = str(input("¿Quieres hacer otro calculo Si o No?: "))
+    except ValueError:
+        print(" Si o No: 🤔 ")
+        continue
+    if otro_calculo != "No" or otro_calculo != "no":
+        print("😊 Nueva conversión 😊")
     elif otro_calculo == "No" or otro_calculo == "no":
-        print("Gracias por usar mi programa :D")
+        print("😍😍😍 Gracias por usar mi programa 😍😍😍")
+    elif otro_calculo == "NO" or otro_calculo == "n":
+        print("😍😍😍 Gracias por usar mi programa 😍😍😍")
         break
