09d14875476d8d0e986a692ef53d7bf8a964aa06
@afop <felipeorozcopinilla@gmail.com>
2022-11-10 20:01:29 -0500

    Ajuste de PEP

--- 5000 DE 5 EN 5.py
+++ 5000 DE 5 EN 5.py
@@ -1,7 +1,4 @@
-#for i in range(1000,10001,5):
- #   if i == 0:
-  #      continue
-   # print(i)
-numbersde5en5=[i for i in range(1000,5000,5)]
-print(numbersde5en5)
-print(len(numbersde5en5))
+
+
+numbersde5en5 = [i for i in range(1000, 5000, 5)]
+print(tuple(numbersde5en5))
