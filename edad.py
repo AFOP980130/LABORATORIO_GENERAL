2f6430f1ceec0b40754436cdd401eb6efd00c6a6
AFOP980130 <felipeorozcopinilla@gmail.com>
2022-11-10 20:44:37 -0500

    Ajustes PEP

--- edad.py
+++ edad.py
@@ -1,4 +1,4 @@
-"Bienvenido a mi 3er programa de medidor de edad legal"
+"""Bienvenido a mi 3er programa de medidor de edad legal"""
 edadlegal = 18
 while True:
     try:
@@ -6,22 +6,20 @@ while True:
     except ValueError:
         print("Debes escribir un número.")
         continue
-    if  edadusuario == edadlegal:
+    if edadusuario == edadlegal:
         print("Eres ahora mayor de edad")
     elif edadusuario > edadlegal:
         edadmayor = (edadusuario - edadlegal)
         print("Ya tienes " + str(edadmayor) + " años más que la edad legal")
     elif edadusuario < edadlegal:
         edadmenor = (edadlegal - edadusuario)
-        print( "Aún te faltan " + str(edadmenor) + " años para llegar a la edad legal")
+        print("Aún te faltan " + str(edadmenor) + " años para llegar a la edad legal")
     else:
         print("debes ingresar un edad en número")
 
     nuevocalculo = (str(input("¿Quieres saber si tienes las edad legar? : ")))
-    if nuevocalculo == "si" or  nuevocalculo == "SI":
+    if nuevocalculo == "si" or nuevocalculo == "SI":
         print("Nuevo Calculo")
-    elif nuevocalculo == "No" or nuevocalculo =="no":
+    elif nuevocalculo == "No" or nuevocalculo == "no":
         print("Gracias por usar mi tercer programa")
         break
-
-
