dadef7f07ca3c2286bce3bf0cab51cc3da04ad4d
@afop <felipeorozcopinilla@gmail.com>
2022-11-10 19:57:40 -0500

    Ajuste de PEP

--- import.py
+++ import.py
@@ -1,34 +1,36 @@
 import random
 
-def run ():
+
+def run():
     bienvenida = """
     Vamos al juego de la adivinanza, trata de adivinar el número que la computadora eligio.
     ES DEL 1 AL 100
     TIENES 5 VIDAS 
     """
     print(bienvenida)
-    numerovariado = random.randint(1,100)
+    numerovariado = random.randint(1, 100)
     numeroelegido = int(input("Escribe tu número: "))
     vidas = 5
-    run2(numerovariado,numeroelegido, vidas)
-def run2( numerovariado, numeroelegido, vidas):
+    run2(numerovariado, numeroelegido, vidas)
+
+
+def run2(numerovariado, numeroelegido, vidas):
     while numeroelegido != numerovariado:
         if vidas == 0:
             print("Perdiste el número era: ", numerovariado, "👀")
             run()
         print("Te quedan:", vidas, " vidas.")
-        if numeroelegido<numerovariado:
+        if numeroelegido < numerovariado:
             vidas -= 1
             print("Elige un número mayor ⬆️")
-            numeroelegido=int(input("Escribe tu número: "))
-        elif numeroelegido>numerovariado:
+            numeroelegido = int(input("Escribe tu número: "))
+        elif numeroelegido > numerovariado:
             vidas -= 1
             print("Elige un número menor ⬇️")
             numeroelegido = int(input("Escribe tu número: "))
     if numeroelegido == numerovariado:
         print("Ganaste 😍")
         run()
-
 
 
 if __name__ == "__main__":
