3126e6643700c77eee5c2c85bedc0525616381bf
Andres Felipe Orozco <115897901+AFOP980130@users.noreply.github.com>
2022-11-13 19:35:23 +0000

    Renombrado

--- lab3.py
+++ lab3.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+link = ("https://docs.google.com/spreadsheets/d/1x5zsd27AT0ySLKlkJFmGlBDLA2pTJ93Q-8f07wqpH4U/export?format=csv")
 
-df = pd.read_csv("https://docs.google.com/spreadsheets/d/1x5zsd27AT0ySLKlkJFmGlBDLA2pTJ93Q-8f07wqpH4U/export?format=csv")
+df = pd.read_csv(link)
 
 print(df)
 
