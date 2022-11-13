b0f1d1b6b44b32f4bd8689c88f65504094aa5c28
Andres Felipe Orozco <115897901+AFOP980130@users.noreply.github.com>
2022-11-13 18:57:54 +0000

    PEP

--- 5000 DE 5 EN 5.py
+++ 5000 DE 5 EN 5.py
@@ -1,4 +1,15 @@
 
 
-numbersde5en5 = [i for i in range(1000, 5000, 5)]
-print(tuple(numbersde5en5))
+def run():
+    presentacion = """Vamos a imprimir los números
+     desde el 1000 hasta en 5000 de 5 en 5"""
+    print(presentacion)
+    run2()
+
+
+def run2():
+    numbersde5en5 = [i for i in range(1000, 5000, 5)]
+    print(numbersde5en5)
+
+if __name__ == '__main__':
+    run()
