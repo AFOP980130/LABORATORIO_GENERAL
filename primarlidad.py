6e392af1aca9c890531320da9e7b37506e790375
AFOP980130 <felipeorozcopinilla@gmail.com>
2022-11-12 19:03:34 -0500

    update

--- primarlidad.py
+++ primarlidad.py
@@ -16,13 +16,17 @@ def runa():
     if cantidadh == 0:
         print("No se encontró la H")
     else:
-        print("Se encontró la H", cantidadh , "veces en tu palabra")
-def run3(primo,numero):
+        print("Se encontró la H", cantidadh, "veces en tu palabra")
+
+
+def run3(primo, numero):
     if primo == 0:
-        print("Tu número ",numero, "es primo")
+        print("Tu número ", numero, "es primo")
     else:
         print(numero, "no es primo")
     run()
+
+
 def run2(numero):
     primo = 0
     for i in range(1, numero + 1):
@@ -30,10 +34,13 @@ def run2(numero):
             continue
         if numero % i == 0:
             primo += 1
-    run3(primo,numero)
+    run3(primo, numero)
+
+
 def run():
     numero = int(input("Escribe un número para saber si es primo o no: "))
     run2(numero)
+
 
 if __name__ == "__main__":
     run()
